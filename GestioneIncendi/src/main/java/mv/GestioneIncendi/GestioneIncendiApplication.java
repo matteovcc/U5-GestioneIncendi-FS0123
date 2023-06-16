75fe1296c18b06c04293b79f1b1575cfbe4edf35
matteovcc <matteovcc99@gmail.com>
2023-06-16 09:48:33 +0200

    start

---
+++ GestioneIncendi/src/main/java/mv/GestioneIncendi/GestioneIncendiApplication.java
@@ -0,0 +1,13 @@
+package mv.GestioneIncendi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneIncendiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneIncendiApplication.class, args);
+	}
+
+}
